9d1003ce0a74ffb9310b3d0ffa2b98ab5aacd6b2
Naman Gupta <namangupta@Namans-MacBook-Air-3.local>
2025-06-01 19:52:30 +0530

    Added files

--- app.py
+++ app.py
@@ -30,4 +30,5 @@ def index():
     return render_template("index.html", prompt=prompt)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 locally
+    app.run(host="0.0.0.0", port=port)
